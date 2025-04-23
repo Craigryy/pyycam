c2a759634758f562883eb8001320a0b09a7f05b1
Craigryy <harria.hj@gmail.com>
2025-04-23 17:14:46 +0100

    added ci/cd

---
+++ infra/setup/variable.tf
@@ -0,0 +1,20 @@
+variable "tf_state_bucket" {
+  description = "Name of S3 bucket in AWS for storing TF state"
+  default     = "devops-pycam-app-state"
+}
+
+variable "tf_state_lock_table" {
+  description = "Name of DynamoDB table for TF state locking"
+  default     = "devops-pycam-app-tf-lock"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default     = "recipe-app-api"
+}
+
+variable "contact" {
+  description = "Contact name for tagging resources"
+  default     = "harriajames98@gmail.com"
+}
+
